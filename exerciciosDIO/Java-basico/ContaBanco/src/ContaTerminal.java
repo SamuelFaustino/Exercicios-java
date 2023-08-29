55210237f9a234ca535c1bf87b5496d20ff1d239
SamuelFaustino <samuelfaustino201@gmail.com>
2023-08-28 21:16:21 -0300

    alt desafio 01

--- exerciciosDIO/Java-basico/ContaBanco/src/ContaTerminal.java
+++ exerciciosDIO/Java-basico/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        int numeroCc;
+        String nome, agencia;
+        Double saldo;
+
+        System.out.println("--- Cadastro de Conta ---");
+        System.out.println("Digite o nome do Titular: ");
+        nome = scan.next();
+
+        System.out.println("Digite o número da Conta: ");
+        numeroCc = scan.nextInt();
+
+        System.out.println("Digite a Agencia: ");
+        agencia = scan.next();
+
+        System.out.println("Digite o valor depositado: ");
+        saldo = scan.nextDouble();
+
+        System.out.println("Olá "+nome+" Obrigado por criar uma conta em nosso banco, sua agência é " + agencia+
+                            " , sua conta é "+numeroCc+" e o seu saldo: "+ saldo+" ,Ja está disponível para saque!");
     }
 }
